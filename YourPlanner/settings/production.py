89763cb9da0432070a72bdaa7b4cd7123b67c83a
chrys <zampas@gmail.com>
2025-05-23 20:23:23 +0100

    production

---
+++ YourPlanner/settings/production.py
@@ -0,0 +1,59 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['www.fasolaki.com']
+
+# Security settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Static/Media files
+STATIC_URL = '/yourplanner/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/yourplanner/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yourplanner',
+        'USER': 'planner',
+        'PASSWORD': 'PlannerPlanner2025!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'journal': {
+            'level': 'INFO',
+            'class': 'systemd.journal.JournalHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['journal'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['journal'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
